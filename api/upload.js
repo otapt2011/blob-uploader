89e117a095924e476a447d99a76d00bc2c385929
JaferF <otapt2011@gmail.com>
2026-08-04 02:10:42 +0300

    Create upload.js

---
+++ api/upload.js
@@ -0,0 +1,17 @@
+import { put } from "@vercel/blob";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get("filename");
+
+  if (!filename) {
+    return NextResponse.json({ error: "Missing filename" }, { status: 400 });
+  }
+
+  const blob = await put(filename, request.body, {
+    access: "private",
+  });
+
+  return NextResponse.json(blob);
+}

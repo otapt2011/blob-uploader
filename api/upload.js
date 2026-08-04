bea7132a3fdbfa5118d797dd372e9c1238a21d4a
JaferF <otapt2011@gmail.com>
2026-08-04 04:14:23 +0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,6 @@
 import { put } from '@vercel/blob';
 
-const TOKEN = "vercel_blob_rw_2R41OxKmYZk4u3TO_AibX9MaGZWlw9A4PK47lzcxY24kAp6"; 
+const TOKEN = "vercel_blob_rw_Gk9GPvdVvphlILt6_6kTmdBNw8NuEjPvsJroF9cBitGsiIt"; 
 
 export async function POST(request) {
   const { searchParams } = new URL(request.url);
